a31ec435749324bde77f3131475e594e39979fd2
vanessasharma29 <vanessasharma29@gmail.com>
2024-01-24 09:05:22 -0500

    Initial1 demo1

---
+++ src/git_demo_wednesday1/Git_Demo_Wednesday1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package git_demo_wednesday1;
+
+/**
+ *
+ * @author vanes
+ */
+public class Git_Demo_Wednesday1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
